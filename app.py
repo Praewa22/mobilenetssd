65ae1fc39fe2f88620e2707d783ff712a7bc87ea
Funn <89174142+FunnXix@users.noreply.github.com>
2021-09-30 10:55:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'O47wD8bve9h7veg5pPbOzk2AV3hBerXsm5JWJKNjd5tx7KYL3ol2ekBWHNQKyCLHTOcgTOEtzLQplHvORZAegAP7lyMpkEiKAPA3ZIXkowDgQHACcxeAa7ixyfGmgTygV+7B5b1vrt+GOlEcL64x3wdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
