d44ca150eb56492c9e0de1c23ec8ab0a5c305456
Praewa22 <89088108+Praewa22@users.noreply.github.com>
2021-11-21 16:58:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def event_handle(event):
             '''
             replyObj = TextSendMessage(text=json_headers)
             line_bot_api.reply_message(rtoken, replyObj)
-      elif msgType == "image":
+    elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
             i = Image.open(BytesIO(message_content.content))
